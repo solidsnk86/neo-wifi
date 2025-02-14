ef8a9c0b05203a8737b70af1bfb78cc0f853aa21
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2025-02-14 06:56:09 -0300

    fix function

--- src/utils/get-ip.ts
+++ src/utils/get-ip.ts
@@ -1,6 +1,7 @@
 export const getIP = async () => {
   const response = await fetch("https://solid-geolocation.vercel.app/location");
   const data = await response.json();
-  const { ip, sysInfo, emojiFlag } = data;
+  const { ip, sysInfo, country } = data;
+  const emojiFlag = country.emojiFlag;
   return { ip, sysInfo, emojiFlag };
 };

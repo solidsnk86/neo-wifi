7972a56adc12022216d77c97f45187cc970e9975
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2025-02-15 13:22:34 -0300

    updated style

--- src/utils/dialog.ts
+++ src/utils/dialog.ts
@@ -1,13 +1,34 @@
 export const showDialog = ({ content }: { content: string }) => {
+  if (!content) return;
+
   const dialog = document.createElement("dialog");
   dialog.innerHTML = `<div>${content}</div>`;
-  if (content !== "undefined") {
-    document.body.appendChild(dialog);
-    dialog.showModal();
-  }
-  document.addEventListener("click", (event: MouseEvent) => {
-    if (dialog && dialog.contains(event.target as Node)) {
-      dialog.close();
-    }
-  });
+  document.body.appendChild(dialog);
+  dialog.showModal();
+
+  const closeDialogWithAnimation = () => {
+    if (dialog.getAttribute("data-closing") === "true") return;
+    dialog.setAttribute("data-closing", "true");
+
+    dialog.style.animation = "slideOutEffect 300ms ease-in-out";
+
+    dialog.addEventListener(
+      "animationend",
+      () => {
+        dialog.close();
+        dialog.remove();
+      },
+      { once: true }
+    );
+  };
+
+  document.addEventListener(
+    "click",
+    (event: MouseEvent) => {
+      if (dialog.contains(event.target as Node)) {
+        closeDialogWithAnimation();
+      }
+    },
+    { once: true }
+  );
 };

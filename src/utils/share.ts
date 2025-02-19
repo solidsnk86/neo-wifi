6e6d19e7f0a40d1213628074323f6671270d6f5e
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2025-02-19 20:11:11 -0300

    updated sharer

--- src/utils/share.ts
+++ src/utils/share.ts
@@ -1,10 +1,13 @@
+const sharer = () =>
+  navigator.share({
+    title: document.title,
+    text: "Configura tu WiFi de manera automatizada en segundos con esta aplicación! 🚀",
+    url: typeof window !== "undefined" ? window.location.href : location.href,
+  });
+
 export const share = () => {
   if ("share" in navigator) {
-    navigator.share({
-      title: document.title,
-      text: "Configura tu WiFi de manera automatizada en segundos con esta aplicación! 🚀",
-      url: typeof window !== "undefined" ? window.location.href : location.href,
-    });
+    sharer();
   } else {
     throw new Error(`Navigartor doesn't support share`);
   }

8eeafbd4e9926996d851f57cda882408e50639a8
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2025-03-02 23:14:43 -0300

    updated link

--- src/app/components/DownloadButton/DownloadButton.tsx
+++ src/app/components/DownloadButton/DownloadButton.tsx
@@ -1,15 +1,16 @@
 import { Download } from "lucide-react";
+import Link from "next/link";
 import { ComponentProps } from "react";
 
 export const DownloadButton = (props: ComponentProps<"button">) => {
   return (
     <button {...props}>
-      <a
-        href="https://mpago.la/1n9xwub"
+      <Link
+        href="/download"
         className="flex items-center gap-3 font-semibold text-lg"
       >
         <Download /> Descargar
-      </a>
+      </Link>
     </button>
   );
 };

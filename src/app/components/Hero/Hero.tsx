dd765a37d0e531eb4068d6d5a7214c6fc9211dac
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2025-03-05 18:37:36 -0300

    updated button style

--- src/app/components/Hero/Hero.tsx
+++ src/app/components/Hero/Hero.tsx
@@ -29,7 +29,7 @@ export const Hero = () => {
         </Link>
         <Link
           href="/download"
-          className="py-3 px-4 border border-zinc-300/70 dark:border-zinc-800 rounded-full text-white dark:text-zinc-900 bg-zinc-800 dark:bg-zinc-100 cursor-pointer flex items-center gap-2 group hover:opacity-80"
+          className="py-3 px-4 cursor-pointer flex items-center gap-2 group hover:opacity-80"
         >
           Comenzar
           <ArrowRight className="group-hover:translate-x-1 transition-transform duration-300" />

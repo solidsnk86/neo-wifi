167e3a66c2e325d148ec7a326692017b17e15ccb
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2025-03-10 21:52:32 -0300

    updated component

--- src/app/components/GeoInfo/components/InfoWifi.jsx
+++ src/app/components/GeoInfo/components/InfoWifi.jsx
@@ -6,11 +6,13 @@ export const InfoWifi = ({ location, loading }) => {
   return (
     <div className="border bg-[#FFFFFF] dark:bg-zinc-800/50 border-zinc-200/70 dark:border-zinc-800 p-3 rounded-2xl relative text-text-primary overflow-hidden">
       <h2
-        title="Información válida para la provincia de San Luis"
+        title={`Información válida para la provincia de ${
+          location.state || "Sin localización"
+        }`}
         className="font-semibold text-lg md:text-2xl py-3 px-3 items-center flex gap-2 justify-start text-left"
       >
         <Wifi className="w-10 h-10 py-1 px-2 border border-zinc-200/70 dark:border-zinc-700/50 rounded-xl bg-gradient-to-b from-zinc-300/10 to-zinc-600/10 text-green-500" />
-        Información WiFi Gob. San Luis
+        Información WiFi Gob. {location.state || "Sin localización"}
       </h2>
       <div className="p-4">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">

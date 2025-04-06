2b4b91063ea5251e31bee2d9b871a5ec52ec6bf4
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2025-04-06 01:25:56 -0300

    chore: added new description app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Neo-Wifi",
   description:
-    "Obtiene información de tu WiFi 📡 más cercano y a que distancia te encuentras del más cercano en tu ciudad con ésta aplicación y configura de manera automatizada cualquier dispositivo inlámbrico TP-LINK CPE con la app de escritorio.",
+    "Detecta la red WiFi 📡 más cercana y conoce tu distancia a ella. Configura automáticamente cualquier dispositivo TP-LINK CPE con nuestra app de escritorio.",
   openGraph: {
     type: "website",
     url: "https://neo-wifi.vercel.app/",

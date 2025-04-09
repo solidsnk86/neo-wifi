d66a7990d954633210860df4454b03c250175072
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2025-04-09 06:22:57 -0300

    chore: updated search text card

--- src/app/components/GeoInfo/components/SearchAntenna.tsx
+++ src/app/components/GeoInfo/components/SearchAntenna.tsx
@@ -30,20 +30,14 @@ export const SearchAntenna = ({
     <div className="border-2 mt-5 bg-[#FFFFFF] dark:bg-zinc-800/50 border-zinc-200/70 dark:border-zinc-800 rounded-2xl relative text-text-primary backdrop-blur-xl z-50">
       <article className="border-b-4 border-zinc-300 dark:border-[#111111] rounded-xl p-3">
         <form onSubmit={submit} className="flex flex-col gap-2">
-          <h2
-            title="Información válida para la provincia de San Luis"
-            className="font-semibold text-lg md:text-2xl py-3 px-3 items-center flex gap-2 justify-start"
-          >
+          <h2 className="font-semibold text-lg md:text-2xl py-3 px-3 items-center flex gap-2 justify-start">
             <span className="w-10 h-10 py-1 px-2 flex items-center justify-center border border-zinc-200/70 dark:border-zinc-700/50 rounded-xl bg-gradient-to-b from-zinc-300/10 to-zinc-600/10 text-zinc-600">
               📡
             </span>
             Buscar antena
           </h2>
           <p className="mb-3 text-pretty text-center">
-            Si no puedes encontrar tu antena más cercana disponible, puedes
-            utilizar el mapa interactivo para buscar los nombres de las antenas
-            cercanas. De esta manera, podrás conocer la distancia a la que te
-            encuentras de cada una de ellas.
+            Podés buscar un antena acá para saber a que distancia estás.
           </p>
           <div className="md:flex grid justify-center mx-auto md:gap-4 gap-2">
             <input

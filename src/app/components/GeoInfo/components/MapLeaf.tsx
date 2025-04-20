1ac17cb9f39b1ec5ce8771217ae89312ce08bcea
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2025-04-20 08:42:18 -0300

    chore: updated titles

--- src/app/components/GeoInfo/components/MapLeaf.tsx
+++ src/app/components/GeoInfo/components/MapLeaf.tsx
@@ -163,7 +163,7 @@ const LeafMap = ({
 
       div.innerHTML = `
         <button class="leaflet-control-locate-btn" title="Centrar en mi ubicación">
-          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-map-pin-icon lucide-map-pin"><path d="M20 10c0 4.993-5.539 10.193-7.399 11.799a1 1 0 0 1-1.202 0C9.539 20.193 4 14.993 4 10a8 8 0 0 1 16 0"/><circle fill="#fff" cx="12" cy="10" r="3"/></svg>
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#111" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-map-pin-icon lucide-map-pin"><path d="M20 10c0 4.993-5.539 10.193-7.399 11.799a1 1 0 0 1-1.202 0C9.539 20.193 4 14.993 4 10a8 8 0 0 1 16 0"/><circle fill="#fff" cx="12" cy="10" r="3"/></svg>
         </button>
       `;
 
@@ -230,7 +230,7 @@ const LeafMap = ({
       }
 
       div.innerHTML = `
-        <button class="map-control-btn" title="Cambiar vista a Mapa">
+        <button class="map-control-btn" title="Vista de Mapa">
          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-map-icon lucide-map"><path d="M14.106 5.553a2 2 0 0 0 1.788 0l3.659-1.83A1 1 0 0 1 21 4.619v12.764a1 1 0 0 1-.553.894l-4.553 2.277a2 2 0 0 1-1.788 0l-4.212-2.106a2 2 0 0 0-1.788 0l-3.659 1.83A1 1 0 0 1 3 19.381V6.618a1 1 0 0 1 .553-.894l4.553-2.277a2 2 0 0 1 1.788 0z"/><path d="M15 5.764v15"/><path d="M9 3.236v15"/></svg>
         </button>
       `;
@@ -249,7 +249,7 @@ const LeafMap = ({
       }
 
       div.innerHTML = `
-        <button class="satellite-control-btn" title="Cambiar vista a Satélite">
+        <button class="satellite-control-btn" title="Vista de Satélite">
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-satellite-icon lucide-satellite"><path d="M13 7 9 3 5 7l4 4"/><path d="m17 11 4 4-4 4-4-4"/><path d="m8 12 4 4 6-6-4-4Z"/><path d="m16 8 3-3"/><path d="M9 21a6 6 0 0 0-6-6"/></svg>
         </button>
       `;

c7439e40991f7da35a64d18403cc0940395f8f25
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2025-04-20 08:32:27 -0300

    fix: remove method from api

--- src/app/api/cpe-info/route.ts
+++ src/app/api/cpe-info/route.ts
@@ -1,15 +1,5 @@
-import { CpeInfoProps } from "@/types/definitions";
-
 export async function GET() {
   try {
-    const getCpeInfo = async () => {
-      process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
-      const response = await fetch(
-        "https://192.168.0.254/data/info.json?id=info"
-      );
-      const data = (await response.json()) as Promise<CpeInfoProps>;
-      return data;
-    };
     const getAppVersion = async () => {
       const response = await fetch(
         "https://api.github.com/repos/solidsnk86/neo-wifi/tags"
@@ -18,17 +8,9 @@ export async function GET() {
       return dataVer;
     };
 
-    const [cpeInfo, appVersion] = await Promise.all([
-      getCpeInfo(),
-      getAppVersion(),
-    ]);
+    const [appVersion] = await Promise.all([getAppVersion()]);
 
     return Response.json({
-      mode: cpeInfo.mode,
-      username: cpeInfo.username,
-      is2G: cpeInfo.is2G,
-      devInfo: cpeInfo.devInfo,
-      devVer: cpeInfo.devVer,
       version: appVersion[0].name || "No disponible",
     });
   } catch (error) {

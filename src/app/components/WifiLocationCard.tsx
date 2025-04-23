46d0be3e0f94c8069dcf90e5098f7c9db46e1609
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2025-04-23 14:07:10 -0300

    style: changed icon

--- src/app/components/WifiLocationCard.tsx
+++ src/app/components/WifiLocationCard.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { getCountryFlag } from "@/utils/convert-to-flag";
-import { MapPinCheck } from "lucide-react";
+import { MapPinnedIcon } from "lucide-react";
 import { Children, cloneElement, ReactNode, useCallback } from "react";
 
 const locations = [
@@ -50,7 +50,7 @@ export default function WifiLocationsCard() {
               className="flex items-center gap-4 p-4 w-[260px] rounded-2xl bg-gradient-to-b from-zinc-50/60 to-zinc-200 dark:from-zinc-800/50 dark:to-zinc-900 cursor-default border border-zinc-200/70 dark:border-zinc-800/50 backdrop-blur-md transition hover:scale-[1.02] hover:shadow-lg"
             >
               <div className="p-3 rounded-full bg-slate-100 dark:bg-zinc-800">
-                <MapPinCheck className="text-blue-500" />
+                <MapPinnedIcon className="text-blue-500" />
               </div>
               <div>
                 <h3 className="text-lg font-semibold text-zinc-700 dark:text-zinc-100">

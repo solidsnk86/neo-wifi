8bc2b6e744c94f669e77836caf4c125e2a3be9c6
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2025-04-27 08:34:21 -0300

    feat: updated colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -66,7 +66,7 @@ export default function Home() {
         <HomeBlockTitle>Características</HomeBlockTitle>
         <HomeBlock className="overflow-hidden px-3">
           <div className="flex p-6 rounded-[33px] border-y-2 border-l-2 border-zinc-200/70 dark:border-zinc-800 relative bg-[#FFFFFF] dark:bg-zinc-800/50 backdrop-blur-lg">
-            <span className="absolute -top-9 left-[50%] text-center w-56 -translate-x-[50%] p-3 bg-gradient-to-b from-blue-500 to-blue-700 text-white font-semibold rounded-3xl text-xl">
+            <span className="absolute -top-9 left-[50%] text-center w-56 -translate-x-[50%] p-3 bg-zinc-800 dark:bg-zinc-100 text-white dark:text-black font-semibold rounded-3xl text-xl">
               Puntos WiFi cerca
             </span>
             <Image
@@ -81,7 +81,7 @@ export default function Home() {
               <MousePointer2
                 width={65}
                 height={65}
-                className="rotate-90 text-blue-500"
+                className="rotate-90 text-yellow-300"
               />
             </span>
           </div>

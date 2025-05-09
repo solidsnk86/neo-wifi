07572ea1501196ab67d8ffbb8649d7fbc2027547
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2025-05-09 20:42:49 -0300

    feat: updated responsive btn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -115,11 +115,11 @@ export default function Home() {
         </article>
 
         <section className="overflow-hidden relative">
-          <p className="flex justify-center mx-auto text-center text-xl mt-6 bg-[#FFFFFF] dark:bg-zinc-950/50 w-fit p-3 rounded-t-xl backdrop-blur-xl relative z-50 cursor-default border-x-2 border-t-2 border-zinc-200/70 dark:border-zinc-900/50">
+          <p className="flex justify-center mx-auto text-center text-xl mt-6 bg-[#FFFFFF] dark:bg-zinc-950/50 w-fit p-3 md:rounded-t-xl backdrop-blur-xl relative z-50 cursor-default border-x-2 border-t-2 border-zinc-200/70 dark:border-zinc-900/50">
             Redes WiFi disponibles en las siguientes localidades
           </p>
           <button
-            className="absolute right-2 top-8 bg-[#FFFFFF] dark:bg-zinc-950/50 p-2 rounded-full hover:brightness-125 backdrop-blur-lg z-50 border-2 border-zinc-200/70 dark:border-zinc-900/50"
+            className="absolute right-2 md:top-8 top-1 bg-[#FFFFFF] dark:bg-zinc-950/50 p-2 rounded-full hover:brightness-125 backdrop-blur-lg z-50 border-2 border-zinc-200/70 dark:border-zinc-900/50"
             onClick={handleClikStateBtn}
           >
             {isPaused ? <Play /> : <Pause />}

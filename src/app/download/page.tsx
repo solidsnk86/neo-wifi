9edf746da5e45334e59abf9f245e6e54179f41b2
solidsnk86 <calcagni.gabriel86@gmail.com>
2025-06-09 06:05:33 -0300

    style: updated margin t

--- src/app/download/page.tsx
+++ src/app/download/page.tsx
@@ -143,7 +143,7 @@ export default function Page() {
 
           {isLoading ? (
             <article className="h-[922px] p-4 mt-12 text-left border bg-[#FFFFFF] dark:bg-zinc-800/50 border-zinc-200/70 dark:border-zinc-800 rounded-2xl z-50 relative backdrop-blur-xl">
-              <div className="flex gap-2 mx-auto items-center justify-center align-middle font-semibold">
+              <div className="flex mt-20 gap-2 mx-auto items-center justify-center align-middle font-semibold">
                 <Loader className="animate-spin text-zinc-800 dark:text-zinc-200" />
                 Cargando...
               </div>

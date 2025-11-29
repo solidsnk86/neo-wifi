4b9933be6b8bd62e24d2f25e322deeee3ed4c992
solidsnk86 <calcagni.gabriel86@gmail.com>
2025-11-29 01:51:30 -0300

    fix: name

--- src/app/api/send-visit/route.ts
+++ src/app/api/send-visit/route.ts
@@ -3,14 +3,13 @@ import { supabase } from "@/supabase/utils/connection";
 export async function POST(req: Request) {
   try {
     const { ip, city, country, latitude, longitude, so, browser, version, emoji_flag } = await req.json();   
-    const payload = { ip, city, country, latitude, longitude, so, browser, version, emoji_flag }
     const { error } = await supabase
-      .from("neo-wifi_visitors")
+      .from("neo_wifi_visitors")
       .insert([
         { ip, city, country, latitude, longitude, so, browser, version, emoji_flag },
       ]);
 
-    if (error) return Response.json({ message: "Error to send data: " + error.message, payload }, { status: 400 });
+    if (error) return Response.json({ message: "Error to send data: " + error.message }, { status: 400 });
 
     return Response.json({ message: "Data registered OK" });
   } catch (error) {

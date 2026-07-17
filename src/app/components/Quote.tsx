8781466bb880da428ad7a30de1197ad18f547c2a
solidsnk86 <calcagni.gabriel86@gmail.com>
2026-07-17 03:39:20 -0300

    chore: changed font

--- src/app/components/Quote.tsx
+++ src/app/components/Quote.tsx
@@ -6,7 +6,7 @@ export default function Quote() {
     <section className="flex mx-auto px-3 max-w-3xl">
       <div className="border-2 border-zinc-200/70 dark:border-zinc-800 rounded-[16px] bg-[#FFFFFF] dark:bg-zinc-800/40 z-50 backdrop-blur-xl">
         <article className="border-b-4 border-zinc-300 dark:border-[#111111] rounded-[14px] p-3">
-          <p className="relative flex text-center text-base md:text-lg font-semibold text-zinc-900 dark:text-zinc-400">
+          <p className="relative flex text-center text-base md:text-lg font-thin text-zinc-900 dark:text-zinc-400 italic">
             <QuoteIcon className="rotate-180 text-zinc-400/70 dark:text-zinc-800 w-10 h-10 fill-yellow-300" />
             Creo que el acceso a una conexión a Internet estable y segura es un
             derecho para todos. Quiero ayudar a potenciar la conectividad y

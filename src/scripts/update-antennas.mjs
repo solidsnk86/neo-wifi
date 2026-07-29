26c731ec5824aa210fe1004166634e7fcbb72420
solidsnk86 <calcagni.gabriel86@gmail.com>
2026-07-29 13:47:00 -0300

    fix import path

--- src/scripts/update-antennas.mjs
+++ src/scripts/update-antennas.mjs
@@ -1,6 +1,6 @@
 import { promises as fs } from "node:fs";
 import path from "path";
-import currentData from "/data/wifi-locates.json" with { type: "json" };
+import currentData from "../../public/data/wifi-locates.json" with { type: "json" };
 import oldDataWifi from "../app/components/GeoInfo/components/data/old-data-wifi.json" with { type: "json" };
 
 (async () => {
